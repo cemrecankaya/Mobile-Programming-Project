d280988025c886e18116f3264f3f0af6f3d096ff
fehmi <fehmiisener@gmail.com>
2021-12-24 12:42:23 +0300

    New schema

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,43 +1,105 @@
 import React, {useEffect} from 'react';
-import {View, SafeAreaView, Alert} from 'react-native';
+import {View, SafeAreaView, Alert,Vibration} from 'react-native';
 import Mybutton from './components/Mybutton';
 import Mytext from './components/Mytext';
 import {openDatabase} from 'react-native-sqlite-storage';
-import PouchDB from 'pouchdb-react-native'
+import call from 'react-native-phone-call';
+import PushNotification, {Importance} from 'react-native-push-notification';
 
 var sqliteDB = openDatabase({name: 'sqlDataBase.db'});
-
-const pouchDb = new PouchDB('mydb')
-let lastID = '65'
+var ws = new WebSocket('wss://echo.websocket.org');
 
 var SharedPreferences = require('react-native-shared-preferences');
-SharedPreferences.setItem("SharedPreferences1","valueOne");
-SharedPreferences.setItem("key2","valueTwo");
-SharedPreferences.setItem("key3","valueThree");
+SharedPreferences.setName("shared_pref");
+//SharedPreferences.clear()
 
-const readAllSharedPreferences = () => { SharedPreferences.getAll(function(values){
-  alert(values);
-});
+const checkIsFirstTime = () => {
+  SharedPreferences.getItem("isLogin", function(value){
+    if(value == null){
+      console.log("First time in app")
+      Alert.alert(
+        "Görünüşe göre aramıza ilk defa katıldın!\n"+
+        "Ekranda bulunan çağrı yap butunu ile bizlere ulaşabilirsin :) "
+     )
+      SharedPreferences.setItem("isLogin","1")
+    }
+    else{
+      console.log("Not first time..")
+    }
+  });
+}
+checkIsFirstTime()
+
+ws.onopen = () => {
+  const message = 'hello';
+  ws.send(message);
+  console.log(`Sent: ${message}`);
+};
+ws.onmessage = (e) => {
+  console.log(`Received Project From Server: ${e.data}`);
+  const message = e.data
+  const message_splitted = fullName.split(' ')
+};
+
+PushNotification.createChannel(
+  {
+    channelId: 'channel-id', 
+    channelName: 'My channel', 
+    channelDescription: 'A channel to categorise your notifications', 
+    playSound: true, 
+    soundName: 'notification.mp3', 
+    importance: Importance.HIGH, 
+    vibrate: true, 
+  },
+  (created) => console.log(`createChannel returned '${created}'`),
+);
+
+const LocalNotification = () => {
+  PushNotification.localNotification({
+    channelId:'channel-id',
+    autoCancel: true,
+    bigText:
+      'This is local notification demo in React Native app. Only shown, when expanded.',
+    subText: 'Local Notification Demo',
+    title: 'Local Notification Title',
+    message: 'Expand me to see more',
+    vibrate: true,
+    vibration: 300,
+    playSound: true,
+    soundName: 'default',
+    actions: '["Yes", "No"]'
+  })
 }
 
-const generateRandomNumber = () => {
-  var randomNumber = Math.floor(Math.random() * 100) + 1;
-  lastID = randomNumber.toString()
-  return randomNumber.toString();
+const ONE_SECOND_IN_MS = 1000;
+
+const PATTERN = [
+  1 * ONE_SECOND_IN_MS,
+  2 * ONE_SECOND_IN_MS,
+  3 * ONE_SECOND_IN_MS
+];
+
+const args = {
+  number: '+905377304283',
+  prompt: true,
+};
+
+const triggerCall = () => {
+  call(args).catch(console.error);
 }
 
 const HomeScreen = ({navigation}) => {
   useEffect(() => {
     sqliteDB.transaction(function (txn) {
       txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='kullanicilar'",
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='tasks'",
         [],
         function (tx, res) {
           console.log('item:', res.rows.length);
           if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS kullanicilar', []);
+            txn.executeSql('DROP TABLE IF EXISTS tasks', []);
             txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS kullanicilar(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(50), user_contact INT(15), user_address VARCHAR(255))',
+              'CREATE TABLE IF NOT EXISTS tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, task_name VARCHAR(50), task_date VARCHAR(50), task_descp VARCHAR(255))',
               [],
             );
           }
@@ -52,38 +114,29 @@ const HomeScreen = ({navigation}) => {
         <View style={{flex: 1}}>
           <Mytext text="RDBMS | SQLite Örneği"/>
           <Mybutton
-            title="Kayıt Ekle"
+            title="Proje Görevi Ekle"
             customClick={() => navigation.navigate('Register')}
           />
           <Mybutton
-            title="Özel Kayıt Görüntüle"
+            title="Proje Görevi Ara"
             customClick={() => navigation.navigate('View')}
           />
           <Mybutton
-            title="Tüm Kayıtları Görüntüle"
+            title="Tüm Görevleri Sorgula"
             customClick={() => navigation.navigate('ViewAll')}
           />
           <Mytext text="NoSQL | PouchDB Örneği"/>
           <Mybutton
-            title="Log Ekle"
-            customClick={() => pouchDb.put({
-              _id : generateRandomNumber(),
-              logTime: new Date().toLocaleString(),
-            }).then(function (response) {
-              console.log(response)
-              console.log(lastID)
-            })}
+            title="Make a Call"
+            customClick={() => triggerCall()}
           />
           <Mybutton
-            title="Log Görüntüle"
-            customClick={() =>
-               pouchDb.get(lastID)
-            .then(doc => alert(doc._id + " - " + doc.logTime))
-          }
+            title="Vibrate with pattern"
+            customClick={() => Vibration.vibrate(PATTERN)}
           />
           <Mybutton
-            title="SharedPreferences Oku"
-            customClick={() => readAllSharedPreferences()}
+            title="Show Notfication"
+            customClick={() => LocalNotification()}
           />
         </View>
       </View>
